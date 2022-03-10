986e0a7348a0a41cb4c24069316d02fc5b47b3d5
matthiascy <matthiasychen@gmail.com>
2022-03-10 20:09:25 +0100

    supress several warnings

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,25 @@ pub mod height_field;
 pub mod isect;
 mod math;
 pub mod mesh;
+
+use crate::error::Error;
+
+#[cfg(not(target_arch = "wasm32"))]
+pub fn run() -> Result<(), Error> {
+    use clap::Parser;
+    use app::VgonioArgs;
+
+    let launch_time = std::time::SystemTime::now();
+
+    // Parse the command line arguments
+    let args: VgonioArgs = VgonioArgs::parse();
+
+    // Initialize vgonio application
+    app::init(&args, launch_time);
+
+    // Dispatch subcommands
+    match args.command {
+        None => app::launch_gui_client(),
+        Some(cmd) => app::execute_command(cmd),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,9 @@
-use vgonio::error::Error;
+#![forbid(unsafe_code)]
+#![warn(clippy::all, rust_2018_idioms)]
 
-fn run() -> Result<(), Error> {
-    use clap::Parser;
-    use vgonio::app;
-    use vgonio::app::VgonioArgs;
-
-    let launch_time = std::time::SystemTime::now();
-
-    // Parse the command line arguments
-    let args: VgonioArgs = VgonioArgs::parse();
-
-    // Initialize vgonio application
-    app::init(&args, launch_time);
-
-    // Dispatch subcommands
-    match args.command {
-        None => app::launch_gui_client(),
-        Some(cmd) => app::execute_command(cmd),
-    }
-}
-
+#[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    ::std::process::exit(match run() {
+    ::std::process::exit(match vgonio::run() {
         Ok(_) => 0,
         Err(ref e) => {
             eprintln!("{}", e);
