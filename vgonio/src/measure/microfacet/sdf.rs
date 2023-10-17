be3b8024d2795b27c4f6a7551a129afcac7e3b04
matthiascy <matthiasychen@gmail.com>
2023-10-17 17:54:35 +0200

    fn signatures

--- vgonio/src/measure/microfacet/sdf.rs
+++ vgonio/src/measure/microfacet/sdf.rs
@@ -2,10 +2,24 @@ use crate::{
     app::cache::{Handle, InnerCache},
     measure::data::MeasurementData,
 };
+use std::path::Path;
+use vgcore::error::VgonioError;
 use vgsurf::MicroSurface;
 
 #[derive(Debug, Clone)]
 pub struct MeasuredSdfData {}
+
+impl MeasuredSdfData {
+    /// Writes the measured data as an EXR file.
+    pub fn write_as_exr(
+        &self,
+        filepath: &Path,
+        timestamp: &chrono::DateTime<chrono::Local>,
+        max_slope: f32,
+    ) -> Result<(), VgonioError> {
+        todo!("write_as_exr")
+    }
+}
 
 pub fn measure_slope_distribution(
     surfaces: &[Handle<MicroSurface>],
