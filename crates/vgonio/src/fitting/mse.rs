cb4df3050e66e9eff5dec41256dd86d359029ac1
matthiascy <matthiasychen@gmail.com>
2024-03-01 18:29:46 +0100

    mse with normalization

--- crates/vgonio/src/fitting/mse.rs
+++ crates/vgonio/src/fitting/mse.rs
@@ -1,19 +1,17 @@
 use crate::{
-    app::{
-        cache::{Cache, RawCache},
-        cli::BrdfModel,
-    },
-    measure::{bsdf::MeasuredBsdfData, data::MeasuredData},
+    app::{cache::RawCache, cli::BrdfModel},
+    measure::bsdf::MeasuredBsdfData,
     RangeByStepSizeInclusive,
 };
-use base::math::{spherical_to_cartesian, sqr, Vec3};
+use base::{
+    math::{spherical_to_cartesian, sqr, Vec3, Vec3A},
+    optics::fresnel,
+};
 use bxdf::{
     brdf::{BeckmannBrdfModel, TrowbridgeReitzBrdfModel},
     MicrofacetBasedBrdfModel,
 };
-use core::slice::SlicePattern;
-use rayon::iter::{IntoParallelRefIterator, ParallelBridge, ParallelIterator};
-use std::sync::atomic::AtomicU64;
+use rayon::iter::ParallelIterator;
 
 /// Compute the mean squared error between the measured data and the model.
 ///
@@ -87,49 +85,65 @@ pub fn compute_iso_brdf_mse(
     let max_theta_o = max_theta_o.unwrap_or(95.0).to_radians();
     let mut num_samples = 0.0;
     let mut mses = vec![0.0; models.len()].into_boxed_slice();
-    let mut max_values_measured = -1.0f64;
-    measured.snapshots.iter().for_each(|snapshot| {
-        snapshot.samples.iter().for_each(|samples| {
-            max_values_measured = max_values_measured.max(samples[0] as f64);
-        });
-    });
-    let mut max_values_model = vec![0.0; models.len()].into_boxed_slice();
-    models
+    // Maximum values of the measured samples for each snapshot.
+    let max_values_measured = measured
+        .snapshots
         .iter()
-        .zip(max_values_model.iter_mut())
-        .for_each(|(model, max_value)| {
-            *max_value = model.eval(Vec3::Z, Vec3::Z, &iors_i[0], &iors_o[0]);
-        });
-    // Iterate over the samples and compute the mean squared error.
-    measured.snapshots.iter().for_each(|snapshot| {
-        let wi = spherical_to_cartesian(1.0, snapshot.wi.theta, snapshot.wi.phi);
-        for (samples, patch) in snapshot.samples.iter().zip(partition.patches.iter()) {
-            let sample = samples[0];
-            let wo_sph = patch.center();
-            if wo_sph.theta.as_f64() > max_theta_o {
-                continue;
-            }
-            let wo = spherical_to_cartesian(1.0, wo_sph.theta, wo_sph.phi);
-            let mut sqr_err = vec![0.0; models.len()].into_boxed_slice();
-            // Compute the squared error for each model per sample.
+        .map(|snapshot| {
+            snapshot.samples.iter().fold(0.0f64, |m, spectral_samples| {
+                m.max(spectral_samples[0] as f64)
+            })
+        })
+        .collect::<Vec<_>>()
+        .into_boxed_slice();
+    // Compute the maximum values of the modelled samples.
+    let mut max_values_model = measured
+        .snapshots
+        .iter()
+        .map(|snapshot| {
+            let wi = spherical_to_cartesian(1.0, snapshot.wi.theta, snapshot.wi.phi);
+            let wo: Vec3 = fresnel::reflect(Vec3A::from(-wi), Vec3A::Z).into();
             models
                 .iter()
-                .zip(sqr_err.iter_mut())
-                .enumerate()
-                .for_each(|(i, (model, sqr_err))| {
-                    *sqr_err = sqr(model.eval(wi, wo, &iors_i[0], &iors_o[0])
-                        / max_values_model[i]
-                        - sample as f64 / max_values_measured);
-                });
-            num_samples += 1.0;
-            // Accumulate the squared error.
-            mses.iter_mut()
-                .zip(sqr_err.iter())
-                .for_each(|(mse, sqr_err)| {
-                    *mse += *sqr_err;
-                });
-        }
-    });
+                .map(|model| model.eval(wi, wo, &iors_i[0], &iors_o[0]))
+                .collect::<Vec<_>>()
+                .into_boxed_slice()
+        })
+        .collect::<Vec<_>>()
+        .into_boxed_slice();
+
+    // Iterate over the samples and compute the mean squared error.
+    measured
+        .snapshots
+        .iter()
+        .enumerate()
+        .for_each(|(snap_idx, snapshot)| {
+            let wi = spherical_to_cartesian(1.0, snapshot.wi.theta, snapshot.wi.phi);
+            for (samples, patch) in snapshot.samples.iter().zip(partition.patches.iter()) {
+                let sample = samples[0];
+                let wo_sph = patch.center();
+                if wo_sph.theta.as_f64() > max_theta_o {
+                    continue;
+                }
+                let wo = spherical_to_cartesian(1.0, wo_sph.theta, wo_sph.phi);
+                let mut sqr_err = vec![0.0; models.len()].into_boxed_slice();
+                // Compute the squared error for each model per sample.
+                models.iter().zip(sqr_err.iter_mut()).enumerate().for_each(
+                    |(i, (model, sqr_err))| {
+                        *sqr_err = sqr(model.eval(wi, wo, &iors_i[0], &iors_o[0])
+                            / max_values_model[snap_idx][i]
+                            - sample as f64 / max_values_measured[snap_idx]);
+                    },
+                );
+                num_samples += 1.0;
+                // Accumulate the squared error.
+                mses.iter_mut()
+                    .zip(sqr_err.iter())
+                    .for_each(|(mse, sqr_err)| {
+                        *mse += *sqr_err;
+                    });
+            }
+        });
     // Compute the mean squared error.
     mses.iter_mut().for_each(|mse| {
         *mse /= num_samples;
