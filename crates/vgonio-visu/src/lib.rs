637c67bce965f18d9fd9b6d786fccba9169962d1
matthiascy <matthiasychen@gmail.com>
2024-11-22 20:14:03 +0100

    wip: visu

--- crates/vgonio-visu/src/lib.rs
+++ crates/vgonio-visu/src/lib.rs
@@ -7,9 +7,25 @@ pub mod random;
 pub mod ray;
 pub mod sphere;
 
+#[cfg(feature = "f64")]
+#[allow(non_camel_case_types)]
+pub type float = f64;
+
+#[cfg(not(feature = "f64"))]
+#[allow(non_camel_case_types)]
+pub type float = f32;
+
 pub struct RenderParams<'a> {
     pub camera: &'a camera::Camera,
     pub world: &'a hit::HittableList,
     pub spp: u32,
     pub max_bounces: u32,
+}
+
+trait Float {
+    fn sqrt(self) -> Self;
+    fn abs(self) -> Self;
+    fn min(self, other: Self) -> Self;
+    fn max(self, other: Self) -> Self;
+    fn clamp(self, min: Self, max: Self) -> Self;
 }
